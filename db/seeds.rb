d634448c68a9458db0123e5f3267563c6bac630c
Quentin <quentinbol@gmail.com>
2023-11-16 17:09:12 +0100

    add seeds to populate database with file info

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,11 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+puts "Generating meals in database..."
+
+File.readlines('./recipes-fr.json').each do |line|
+  json = JSON.parse(line)
+
+  json.except!('author_tip', 'budget')
+  meal = Meal.create(json)
+  puts "Created #{meal.name}"
+end
+
+puts "Meals generated!"
